66586883e93b195dc45b6165baf8fad9f43d1690
jimareed <jim.a.reed@gmail.com>
2015-08-03 22:21:01 -0400

    added /smoke-test/run

--- src/start.js
+++ src/start.js
@@ -8,22 +8,37 @@ var fs = require('fs'),
     xml2js = require('xml2js');
 var shell = require('shelljs');
 
-shell.cd('/src');
-shell.exec('nightwatch --test');
-shell.ls('/src/reports/*').forEach(function(file) {
-	console.log('file:' + file);
-	fs.readFile(file, function(err, data) {
-	   parser.parseString(data, function (err, result) {
-	       results = JSON.stringify(result);
-	   });
-	});
-});
-
 var parser = new xml2js.Parser();
 
+var status = 'done';
 
-app.get('/', function (req, res) {
-  res.send('<html><body><p><b>Smoke Test</b></p><p/><p>' + results + '</p></body></html>\n');
+function runTests() {
+	shell.cd('/src');
+	shell.exec('nightwatch --test');
+	shell.ls('/src/reports/*').forEach(function(file) {
+		console.log('file:' + file);
+		fs.readFile(file, function(err, data) {
+		   parser.parseString(data, function (err, result) {
+		       results = JSON.stringify(result);
+		   });
+		});
+	});
+
+}
+
+runTests();
+
+function getResponse() {
+	return '<html><body><p><b>Smoke Test</b></p><p/><p>' + results + '</p></body></html>\n';
+}
+
+app.get('/smoke-test', function (req, res) {
+  res.send(getResponse());
+});
+
+app.get('/smoke-test/run', function (req, res) {
+  runTests();
+  res.send(getResponse());
 });
 
 app.listen(PORT)
